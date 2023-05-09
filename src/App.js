b06df288c6273bf8815913c80d14b26acf968da6
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-09 06:27:13 +0530

    E-commerce homepage and SASS setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import HomePage from "./homepage.component";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
