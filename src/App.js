1e8854705426d678884eddd1a60a3425c072b65b
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-09 21:11:15 +0530

    ADDED Redirection for Signed Users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import HomePage from "./pages/homepage/homepage.component";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { connect } from "react-redux";
 
 import ShopPage from "./pages/shop/shop.component";
@@ -41,13 +41,26 @@ class App extends React.Component {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/shop" element={<ShopPage />} />
-          <Route exact path="/signIn" element={<SignInAndSignUpPage />} />
+          <Route
+            exact
+            path="/signin"
+            element={
+              this.props.currentUser ? (
+                <Navigate replace to={"/"} />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Routes>
       </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
