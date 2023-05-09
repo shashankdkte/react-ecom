bb3d50d784340e221bcf9bf216f0c82b918e1f10
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-09 08:56:09 +0530

    added navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import HomePage from "./pages/homepage.component";
+import { Route, Routes } from "react-router-dom";
+import Hats from "./hats.component";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<Hats />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/hats.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Hats = (props) => {
+  console.log(props.history);
+  return <div>Hats</div>;
+};
+
+export default Hats;
