ee73508dcd803b6071f23355a863d09626f70c8c
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-12 14:32:27 +0530

    Cart Icon Component UI Added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import "./header.styles.scss";
 
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
